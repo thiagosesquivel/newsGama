790e19446487af4c3420105ec7191fa7619945b2
Thiago Esquivel <thiago.sesquivel@gmail.com>
2020-04-18 01:04:46 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+toggleMenu = (event) => {
+  const menu = document.querySelector(".lista-menu");
+  const alvo = event.target;
+
+  desativar = (elemento) => {
+    elemento.classList.remove("botao-ativo");
+    elemento.classList.remove("fa-times");
+    elemento.classList.add("fa-bars");
+    menu.classList.remove("menu-ativo");
+  };
+
+  ativar = (elemento) => {
+    elemento.classList.add("botao-ativo");
+    elemento.classList.add("fa-times");
+    elemento.classList.remove("fa-bars");
+    menu.classList.add("menu-ativo");
+  };
+
+  alvo.classList.contains("botao-ativo") ? desativar(alvo) : ativar(alvo);
+};
